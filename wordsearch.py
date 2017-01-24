ddc4f15ca4429a3658f4f3112f8e5ba7ba62570e
Sully Sullenberger (Matthew) <sully@sadburger.com>
2017-01-23 21:06:46 -0800

    implement word loader

--- wordsearch.py
+++ wordsearch.py
@@ -6,19 +6,24 @@ from string import ascii_lowercase
 
 
 def load_words():
-    pass
+    with open('words.txt', 'r') as f:
+        return set(x.strip().lower() for x in f.readlines())
+
 
 def generate_grid(size):
     pass
 
+
 def find_words(grid, valid_words):
     pass
+
 
 @click.command()
 @click.argument('size', default=12)
 @click.option('--min-length', '-n', default=0)
 def search(size, min_length):
     valid_words = load_words()
+    click.echo('Wordlist contains {} valid words.'.format(len(valid_words)))
     grid = generate_grid(size)
     found = find_words(grid, valid_words)
 
