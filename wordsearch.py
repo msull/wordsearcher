7daa260de20df79eb278624f43ec841ea03a535a
Sully Sullenberger (Matthew) <sully@sadburger.com>
2017-01-23 21:38:29 -0800

    Add word search, implement horizontal and vertical

--- wordsearch.py
+++ wordsearch.py
@@ -25,8 +25,55 @@ def display_grid(grid):
         click.echo(' '.join(row))
 
 
-def find_words(grid, valid_words):
-    pass
+def find_words(search_strs, valid_words):
+    found = list()
+    for word in valid_words:
+        if any(word in x for x in search_strs):
+            found.append(word)
+    return found
+
+
+def get_grid_strings(grid):
+    """Get the set of all valid strings in the grid.
+
+    > get_grid_strings([['a', 'b'], ['c', 'd']])
+    """
+    str_fns = (
+        _horizontal_strings,
+        _vertical_strings,
+        _diagonal_down_right_strings,
+        _diagonal_down_left_strings,
+    )
+    return set(sum(map(lambda x: x(grid), str_fns), []))
+
+
+def _reverse_strs(strs):
+    return list([x[::-1] for x in strs])
+
+
+def _horizontal_strings(grid):
+    """Return all horizontal strs, forwards and backwards."""
+    strs = list()
+    for row in grid:
+        strs.append(''.join(row))
+
+    return strs + _reverse_strs(strs)
+
+
+def _vertical_strings(grid):
+    strs = list()
+    for col in zip(*grid):
+        strs.append(''.join(col))
+
+    return strs + _reverse_strs(strs)
+
+
+def _diagonal_down_right_strings(grid):
+    return []
+
+
+def _diagonal_down_left_strings(grid):
+    return []
 
 
 @click.command()
@@ -37,7 +84,9 @@ def search(size, min_length):
     click.echo('Wordlist contains {} valid words.'.format(len(valid_words)))
     grid = generate_grid(size)
     display_grid(grid)
-    found = find_words(grid, valid_words)
+    grid_strs = get_grid_strings(grid)
+    click.echo(grid_strs)
+    found = find_words(grid_strs, valid_words)
 
     if found:
         click.echo('Found {} words: \n{}'.format(
